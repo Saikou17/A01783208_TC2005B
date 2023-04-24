70a6ab17d4be898303577919e939b67a060c35c2
Saikou17 <a01783208@itesm.mx>
2023-04-24 14:33:43 -0600

    Ejercicios Java 1

---
+++ Web/JavaS/JavaScriptPractica.js
@@ -0,0 +1,5 @@
+//Escribe una función que encuentre el primer carácter de un cadena de texto que no se repite. Prueba tu función con: 'abacddbec'
+
+function firstCharacter(cadena){
+    return alert(cadena[0])
+}
