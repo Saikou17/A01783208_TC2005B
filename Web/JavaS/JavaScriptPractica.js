57516af2e50dfc28b12165ce6fe9ceaecaa0f282
Saikou17 <a01783208@itesm.mx>
2023-04-24 15:00:41 -0600

    Link

--- Web/JavaS/JavaScriptPractica.js
+++ Web/JavaS/JavaScriptPractica.js
@@ -1,5 +1,18 @@
 //Escribe una función que encuentre el primer carácter de un cadena de texto que no se repite. Prueba tu función con: 'abacddbec'
 
 function firstCharacter(cadena){
-    return alert(cadena[0])
+    console.log(cadena[0]);
+    return cadena[0];
+}
+
+//Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números
+
+function bubbleSort(lista){
+    const index = 0
+    let mayor = lista[index];
+    while (index <= lista.lenght){
+        if(mayor>lista[++index]){
+            mayor = lista[index];
+        }
+    }
 }
